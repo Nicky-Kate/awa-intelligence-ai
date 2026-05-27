fe6944da8065b96415c3d244844dbaf67a937f62
Nicky-Kate <deine-github-email>
2026-05-27 19:21:22 +0200

    server komplett repariert

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require("express");
 
 const app = express();
 
-const PORT = process.env.PORT || 10000;
-
 app.get("/", (req, res) => {
-  res.send("AWA Intelligence AI läuft erfolgreich 🚀");
+  res.send("AWA Intelligence AI läuft!");
 });
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server läuft auf Port ${PORT}`);
